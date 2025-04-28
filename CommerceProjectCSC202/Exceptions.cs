688426795eb1e4a8090fd758beb65865c89a2c34
Benjamin Gress <bgpilot16@gmail.com>
2025-04-27 17:01:14 -0700

    Implimented Primary Interface

---
+++ CommerceProjectCSC202/Exceptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommerceProjectCSC202
+{
+    public class InvalidSelectionException : Exception
+    {
+        // Default constructor
+        public InvalidSelectionException() : base("This is the base one")
+        {
+            Console.WriteLine("");
+        }
+
+        // Constructor that takes a custom message
+        public InvalidSelectionException(string message) : base(message)
+        {
+        }
+
+        // Constructor that takes a custom message and inner exception
+        public InvalidSelectionException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
--- CommerceProjectCSC202/Program.cs
+++ CommerceProjectCSC202/Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine("Please select the method you'd like to run, or press EEE to exit.:");
+                Console.WriteLine("1. Get a specific aircraft's info via its callsign.");
+                
+                Console.Write("Insert selection here: ");
+
+                string brug = Console.ReadLine();
+                try
+                {
+                    //exit line
+                    if (brug == "EEE")
+                    {
+                        break;
+                    }
+                    int sel = Convert.ToInt32(brug);
+                    if (sel >= 1 && sel <= 12) // Update that value whenever you add a method:
+                    {
+                        switch (sel)
+                        {
+                            case 1:
+                                
+                                break;
+                            default:
+                                Console.WriteLine("How did you get here???");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        throw new InvalidSelectionException();
+                    }
+                }
+                catch (NotImplementedException exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("That function is not implimented:" + exception.ToString());
+                    //Console.Write("That did not work. Please try again : ");
+                    //brug = Console.ReadLine();
+                }
+                catch (Exception exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("\nThat did not work. Please try again.\n");
+                    Console.Write(exception.ToString());
+                    //brug = Console.ReadLine();
+                }
+            }
         }
     }
 }
